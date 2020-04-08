d8d7df50d2b9b1fe553db9c2583ca40849d51d60
emilyback <emilyback@cpst.co.kr>
2020-04-08 13:00:14 +0900

    검색폼 구현_3번째 엔터를 입력하면 검색 결과가 보인다 FormView가 검색 결과를 보여주는게 아니라 controller에 위임함.

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -1,5 +1,6 @@
 import View from './View.js'
-
+//검색 결과를 보여주지 않아도됨. enter가 눌렸다는 것을 maincontroller에게 알려주고
+//maincontroller가 그 검색 결과를 resultView에서 보여주는것.
 const tag ='[FormView]' //디버깅태그를위해
 
 const FormView = Object.create(View) //실제 뷰객체를 가져옴
@@ -10,6 +11,7 @@ FormView.setup = function (el){ //element를 가져와서 내부적으로 속성
     this.resetEl = el.querySelector('[type=reset]')
     this.showResetBtn(false)
     this.bindEvents()
+    return this
 }
 
 FormView.showResetBtn = function(show = true){
@@ -17,11 +19,15 @@ FormView.showResetBtn = function(show = true){
 }
 
 FormView.bindEvents = function(){
+    this.on('submit', e => e.preventDefault())
     this.inputEl.addEventListener('keyup', e => this.onKeyup(e))
 }
 
-FormView.onKeyup = function(){ //키가 눌릴때 마다 호출
+FormView.onKeyup = function(e){ //키가 눌릴때 마다 호출
+    const enter = 13
     this.showResetBtn(this.inputEl.value.length)
+    if(e.keyCode !== enter) return 
+    this.emit('@submit', {input: this.inputEl.value}) //enter키가 눌리면?
 }
 
 export default FormView
