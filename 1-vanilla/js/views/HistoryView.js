edd3d6e20613aeb404242ef68772a3b3ac9b339a
emilyback <emilyback@cpst.co.kr>
2020-04-09 10:07:56 +0900

    5.최근검색어-1,2 최근검색어, 목록이 탭아래 위치 +목록에서 검색어를 클릭하면 선택된 검색어로 검색결과 화면으로 이동 개발 KeywordView로 HsitoryView를 만듬.

---
+++ 1-vanilla/js/views/HistoryView.js
@@ -0,0 +1,8 @@
+import KeywordView from '../views/KeywordView.js'
+//입력값을 받아서 보여줄 랜더함수 필요
+
+const tag = '[HistoryView]'
+
+const HistoryView = Object.create(KeywordView)
+
+export default HistoryView
