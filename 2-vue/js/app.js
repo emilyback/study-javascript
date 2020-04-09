121028d47faed7e3844b17c91d88894d54e3c036
emilyback <emilyback@cpst.co.kr>
2020-04-09 16:41:03 +0900

    2.검색결과-1 -검색결과가 검색폼 아래 위치 -검색 결과가 보인다 searchModle을 import해서 item의 name과 image값을 가져온다. 검색이 됬음을 알려주는 submitted(bool같음)을 false로 default 해주고 검색값이 있을때 true로 해줌

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,20 +1,33 @@
+import SearchModel from './models/SearchModel.js'
+
 new Vue({
     el: '#app',
     data: {
-        query: '' //입력데이터를 받아서 저장 입력하게 되면 query랑 연결
+        query: '', //입력데이터를 받아서 저장 입력하게 되면 query랑 연결
                     //미리 설정되도 input 엘레벤트에 저장됨.
+        submitted: false,
+        searchResult: []
     },
     methods:{ //실제 돔과 바인딩 할 함수 정의 가능
         onSubmit(e){
-            debugger
+            this.search()
         },
 
-        onKeyup(){
+        onKeyup(e){
            if (!this.query.length)//입력값이 없을 경우,길이가 없당
-            this.onReset()
+            this.restForm()
+        },
+        onReset(e){ //따로 이벤트 안받아서 안에 e가 없음
+           this.restForm()
+        },
+        search(){
+            SearchModel.list().then(data =>{ //data변수의 검색 결과가 올것
+               this.submitted = true //실제 입력값이 있을때 true
+                this.searchResult = data
+            })
         },
 
-        onReset(){ //따로 이벤트 안받아서 안에 e가 없음
+        restForm(){
             this.query = '' //여기서 this는 vue 인스턴스를 말함,따라서 data의 쿼리
             //todo 검색결과를 숨기는...
             debugger
