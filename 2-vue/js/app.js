ee76b48d15cc1528ef1a0c40303a2a640644e20d
emilyback <emilyback@cpst.co.kr>
2020-04-09 17:57:51 +0900

    5.최근검색어-1 History모델을 임포트 하여 그 키워드를 사용하여 최근검색어 목록을 만든다 추천 검색어 목록과 비슷한 형태이다. +날짜와 삭제 버튼을 추가함.

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,5 +1,6 @@
 import SearchModel from './models/SearchModel.js'
 import KeywordModel from './models/KeywordModel.js'
+import HistoryModel from './models/HistoryModel.js'
 
 new Vue({
     el: '#app',
@@ -10,6 +11,7 @@ new Vue({
         tabs: ['추천 검색어', '최근 검색어'],
         selectedTab: '',
         keywords: [],
+        history: [],
         searchResult: []
         
     },
@@ -17,6 +19,7 @@ new Vue({
     created(){
         this.selectedTab = this.tabs[0]
         this.fetchKeyword()
+        this.fetchHistory()
     },
 
     methods:{ //실제 돔과 바인딩 할 함수 정의 가능
@@ -44,6 +47,12 @@ new Vue({
         fetchKeyword(){
             KeywordModel.list().then(data =>{
                 this.keywords = data
+            })
+        },
+
+        fetchHistory(){
+            HistoryModel.list().then(data =>{
+                this.history = data
             })
         },
 
