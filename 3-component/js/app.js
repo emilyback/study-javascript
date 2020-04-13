1314f6d48027d7f8d023554cce0ee5314f4c1738
emilyback <emilyback@cpst.co.kr>
2020-04-13 14:46:18 +0900

    4.TabComponent구현 현재 tab을 알려주는 selectedTab을 바꿔주는 이벤트함수 만듬

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -5,6 +5,7 @@ import HistoryModel from './models/HistoryModel.js'
 import FormComponent from './component/FormComponent.js'
 import ResultComponent from './component/ResultComponent.js'
 import ListComponent from './component/ListComponent.js'
+import TabComponent from './component/TabComponent.js'
 
 new Vue({
   el: '#app',
@@ -21,7 +22,8 @@ new Vue({
   components:{
     'search-form' : FormComponent, //뷰 인스턴스에 사용할 컴포넌트를 설정하는 부분, 실제 사용시엔 search-form 사용(index.html에서)
     'search-result' : ResultComponent,
-    'list': ListComponent
+    'list': ListComponent,
+    'tabs': TabComponent
   },
 
 
